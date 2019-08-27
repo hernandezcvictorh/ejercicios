4d28d9df3b07ef5d1a050c82520fb93dc84caf47
Diego Arrieta <darrieta.lnx@gmail.com>
2019-08-27 17:30:27 -0400

    Correción

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -17,28 +17,37 @@ public class Ejercicios {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        float notas;
+        float nota, notas = 0, acu = 0;
         String nombre, cond;
-        int cont = 0;
+        int cont = 0, n = 0;
         Scanner leer = new Scanner(System.in);
-        int[] prom = new int[5];
+        float[] prom = new float[5];
 
-        do {
+        for (int i = 0; i <= 1; i++) {
+
             System.out.println("ingrese el nombre del alumno: ");
             nombre = leer.next();
             do {
                 System.out.println("desea agregar nota  (si/no)");
                 cond = leer.next();
                 if (cond.equals("si")) {
+
                     do {
                         System.out.println("ingrese la nota (valores desde el 1,0 al 7,0)");
-                        notas = leer.nextFloat();
-                    } while (!(notas >= 1 && notas <= 7));
+                        nota = leer.nextFloat();
+                        n++;
+                        acu = acu + nota;
+                        notas = (acu) / n;
+                    } while (!(nota >= 1 && nota <= 7));
+
                 }
             } while (!(cond.equals("no")));
-            cont++;
-        } while (!(cont == 5));
-
+            acu=0;
+            prom[i] = notas;
+            
+        }
+        System.out.println(prom[0]);
+        System.out.println(prom[1]);
     }
     
     
