5a689a4787c9260a6466839a8a5a3e33bf2b5af1
Diego Arrieta <darrieta.lnx@gmail.com>
2019-08-27 17:28:12 -0400

    New code

--- src/ejercicios/Ejercicios.java
+++ src/ejercicios/Ejercicios.java
@@ -5,6 +5,8 @@
  */
 package ejercicios;
 
+import java.util.Scanner;
+
 /**
  *
  * @author zeuss
@@ -15,7 +17,29 @@ public class Ejercicios {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        float notas;
+        String nombre, cond;
+        int cont = 0;
+        Scanner leer = new Scanner(System.in);
+        int[] prom = new int[5];
+
+        do {
+            System.out.println("ingrese el nombre del alumno: ");
+            nombre = leer.next();
+            do {
+                System.out.println("desea agregar nota  (si/no)");
+                cond = leer.next();
+                if (cond.equals("si")) {
+                    do {
+                        System.out.println("ingrese la nota (valores desde el 1,0 al 7,0)");
+                        notas = leer.nextFloat();
+                    } while (!(notas >= 1 && notas <= 7));
+                }
+            } while (!(cond.equals("no")));
+            cont++;
+        } while (!(cont == 5));
+
     }
+    
     
 }
